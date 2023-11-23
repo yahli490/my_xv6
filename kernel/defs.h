fbe4e71ef88d38d16a53c94b2cfae910f0ea0a35
yahli490 <yahlihecht@mail.tau.ac.il>
2023-11-23 21:53:39 +0200

    most of exe3

--- kernel/defs.h
+++ kernel/defs.h
@@ -168,6 +168,7 @@ int             uvmcopy(pagetable_t, pagetable_t, uint64);
 void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
 void            uvmclear(pagetable_t, uint64);
+void            vmprint(pagetable_t);
 pte_t *         walk(pagetable_t, uint64, int);
 uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
--- kernel/vm.c
+++ kernel/vm.c
@@ -220,6 +220,30 @@ uvmfirst(pagetable_t pagetable, uchar *src, uint sz)
   memmove(mem, src, sz);
 }
 
+
+void rec_vmprint(pagetable_t pagetable, int depth){
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int ppd = 0; ppd < depth; ++ppd) printf(".. "); 
+      printf("..%d: pte %p pa %p\n", i, (uint64)pte, (uint64)child); 
+      rec_vmprint((pagetable_t)child, depth + 1);
+
+    } else if(pte & PTE_V){
+      for (int ppd = 0; ppd < depth; ++ppd) printf(".. "); 
+      printf("..%d: pte %p pa %p\n", i, (uint64)pte, (uint64)PTE2PA(pte)); 
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", (uint64)pagetable); 
+  rec_vmprint(pagetable, 0);
+}
+
 // Allocate PTEs and physical memory to grow process from oldsz to
 // newsz, which need not be page aligned.  Returns new size or 0 on error.
 uint64
